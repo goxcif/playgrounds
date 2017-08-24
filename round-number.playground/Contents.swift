aa42f8c8f23c8adf5b5bf53c54bd34bdd23435c7
goxcif <goxcif@gmail.com>
2017-08-24 16:07:21 +1000

    Added round-number.playground

---
+++ round-number.playground/Contents.swift
@@ -0,0 +1,29 @@
+import Darwin.C.math
+// included by UIKit
+
+//public func roundf(_: Float) -> Float
+//public func round(_: Double) -> Double
+//
+//public func lroundf(_: Float) -> Int
+//public func lround(_: Double) -> Int
+//
+//public func llroundf(_: Float) -> Int64
+//public func llround(_: Double) -> Int64
+
+round(1.3)
+round(1.499999)
+round(1.5)
+round(1.7)
+round(-1.3)
+round(-1.499999)
+round(-1.5)
+round(-1.7)
+
+lroundf(1.3)
+lroundf(1.499999)
+lroundf(1.5)
+lroundf(1.7)
+lroundf(-1.3)
+lroundf(-1.499999)
+lroundf(-1.5)
+lroundf(-1.7)
